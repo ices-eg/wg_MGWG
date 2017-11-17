5d336c1ba0d07f80fe729e1d685df8c7376ceb8f
jonathanderoba <jonathan.deroba@noaa.gov>
2017-11-17 16:15:13 -0500

    Update Compare Runs

--- state-space/helper_code/CompareRuns.R
+++ state-space/helper_code/CompareRuns.R
@@ -32,8 +32,52 @@ print(rho)
 sink()
 ########End SAM fit
 
+#########ASAP fit
+#-----------------------------------------------
+get_asap_retros <- function(asap.name, npeels=7){
+  retro.lst <- list()
+  asap<-dget(paste(topdirect,species,"ASAP",paste0(asap.name,"_000.rdat"),sep="\\"))
+  term.ssb <- asap$SSB
+  term.avgf <- asap$F.report
+  term.rec <- asap$N.age[,1]
+  nyears <- length(term.ssb)
+  ssb.rho.vals <- rep(NA, npeels)
+  avgf.rho.vals <- rep(NA, npeels)
+  rec.rho.vals <- rep(NA, npeels)
+  for (ipeel in 1:npeels){
+    peel <- dget(paste(topdirect,species,"ASAP",paste0(asap.name,"_00",ipeel,".rdat"),sep="\\"))
+    peel.ssb <- peel$SSB
+    peel.avgf <- peel$F.report
+    peel.rec <- peel$N.age[,1]
+    
+    xterm <- term.ssb[nyears - ipeel]
+    xtip <- peel.ssb[nyears - ipeel]
+    ssb.rho.vals[ipeel] <- (xtip - xterm) / xterm
+    
+    yterm <- term.avgf[nyears - ipeel]
+    ytip <- peel.avgf[nyears - ipeel]
+    avgf.rho.vals[ipeel] <- (ytip - yterm) / yterm
+    
+    zterm <- term.rec[nyears - ipeel]
+    ztip <- peel.rec[nyears - ipeel]
+    rec.rho.vals[ipeel] <- (ztip - zterm) / zterm
+  }
+  ssb.rho <- mean(ssb.rho.vals, na.rm = TRUE)
+  avgf.rho <- mean(avgf.rho.vals, na.rm = TRUE)
+  rec.rho <- mean(rec.rho.vals, na.rm = TRUE)
+  
+  retro.lst$ssb.rho.vals <- ssb.rho.vals
+  retro.lst$ssb.rho <- ssb.rho
+  retro.lst$avgf.rho.vals <- avgf.rho.vals
+  retro.lst$avgf.rho <- avgf.rho
+  retro.lst$rec.rho.vals <- rec.rho.vals
+  retro.lst$rec.rho <- rec.rho
+  return(retro.lst)
+}
 
-ASAPname<-paste0(species,"_ASAP_000.RDAT")
-ASAPfit<-dget(paste(topdirect,species,"ASAP",ASAPname,sep="\\"))
+
+ASAPname<-paste0(species,"_ASAP")
+ASAPret<-get_asap_retros(asap.name=ASAPname)
+
 
 
