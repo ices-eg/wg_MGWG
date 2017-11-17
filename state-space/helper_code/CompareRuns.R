050d1b72e2ed98925af58d90f5d835c0fd927b4f
jonathanderoba <jonathan.deroba@noaa.gov>
2017-11-17 16:03:59 -0500

    Recover Compare Runs Helper Code

--- state-space/helper_code/CompareRuns.R
+++ state-space/helper_code/CompareRuns.R
@@ -3,8 +3,34 @@ rm(list=ls(all=T))
 topdirect<-"C:\\Users\\jonathan.deroba\\Documents\\GitHub\\MGWG\\state-space"
 species<-"SNEMAYT"
 
+########do SAM fit
 library(stockassessment)
-SAMfit<-paste(topdirect,species,"SAM",sep="\\")
+fit<-readRDS(file=paste(topdirect,species,"SAM","SAMfit.RData",sep="\\")) 
+
+source(paste(topdirect,species,"SAM","pred.R",sep="\\")) # function to predict 
+
+dat<-fit$data
+conf <- loadConf(dat,paste(topdirect,species,"SAM","model.cfg",sep="\\"))
+par <- defpar(dat,conf)
+
+RES <- residuals(fit)
+RESP <- procres(fit)
+RETRO <- retro(fit, year=7)
+PRED <- predictYears(fit,years=max(fit$data$years)-2:0)
+colnames(PRED)<-sub("obs", "logObs", colnames(PRED))
+
+rho <- mohn(RETRO, lag=0)
+
+tab1 <- cbind(Year=fit$data$years, summary(fit), round(catchtable(fit)))
+colnames(tab1) <- sub("Estimate", "Catch", colnames(tab1))
+write.table(tab1, file=paste(topdirect,species,"SAM","tab1.csv",sep="\\"), sep=",\t", quote=FALSE, row.names=FALSE)
+
+write.table(PRED, file=paste(topdirect,species,"SAM","tab2.csv",sep="\\"), sep=",\t", quote=FALSE, row.names=FALSE)
+
+sink(file=paste(topdirect,species,"SAM","Mohn.txt",sep="\\"))
+print(rho)
+sink()
+########End SAM fit
 
 
 ASAPname<-paste0(species,"_ASAP_000.RDAT")
