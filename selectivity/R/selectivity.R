269e2ad03bdba453744bd09175c6ec88d85a855f
Arni Magnusson <arnima@hafro.is>
2017-11-17 15:34:22 -0500

    Comment out example

--- selectivity/R/selectivity.R
+++ selectivity/R/selectivity.R
@@ -12,22 +12,21 @@ cohortBiomass <- function(Ninit, w, M, Fvec = 0)
   B
 }
 
-# Example
-Ninit <- 136
-w <- c(1.2, 1.7, 2.4, 3.3, 4.3, 5.4, 6.4, 7.4, 8.7, 10.1, 11.6, 14.6)
-names(w) <- 3:14
-M <- 0.2
-B <- cohortBiomass(Ninit, w, M)
-barplot(B)
+## Example
+## Ninit <- 136
+## w <- c(1.2, 1.7, 2.4, 3.3, 4.3, 5.4, 6.4, 7.4, 8.7, 10.1, 11.6, 14.6)
+## names(w) <- 3:14
+## M <- 0.2
+## B <- cohortBiomass(Ninit, w, M)
+## barplot(B)
 
-# For later consideration
-#Fsel <- c(0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1)
-#Ftime <- 0
+## For later consideration
+## Fsel <- c(0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1)
+## Ftime <- 0
 
-#Fcalc <- function(Fsel,Ftime){
-#  c(Fsel%o%Ftime )
-#}
+## Fcalc <- function(Fsel,Ftime){
+##   c(Fsel%o%Ftime )
+## }
 
-#Fm <- Fcalc(Fsel, Ftime)
-#Fm
-
+## Fm <- Fcalc(Fsel, Ftime)
+## Fm
