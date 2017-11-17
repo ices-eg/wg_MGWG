9cb827ac27e9450b2cef5e3733f46ad6e40fdcc3
al-yanez <aleyanez.rubio@gmail.com>
2017-11-17 15:30:13 -0500

    implemented as function cohortBiomass

--- selectivity/R/selectivity.R
+++ selectivity/R/selectivity.R
@@ -1,29 +1,33 @@
-w <- c(1.2, 1.7, 2.4, 3.3, 4.3, 5.4, 6.4, 7.4, 8.7, 10.1, 11.6, 14.6)
-M <- 0.2
-
-Fsel <- c(0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1)
-Ftime <- 0
-
-Fcalc <- function(Fsel,Ftime){
-  c(  Fsel %o% Ftime )
+cohortBiomass <- function(Ninit, w, M, Fvec = 0)
+{
+  len <- length(w)
+  M <- rep(M, length=len)
+  Fvec <- rep(Fvec, length=len)
+  Z <- Fvec + M
+  N <- numeric(len)
+  N[1] <- Ninit
+  for(i in 1:(len-1))
+    N[i+1] <- N[i] * exp(-Z[i])
+  B <- N * w
+  B
 }
 
-Fm <- Fcalc(Fsel, Ftime)
-Fm
-
-Z <- Fm + M
+# Example
 Ninit <- 136
+w <- c(1.2, 1.7, 2.4, 3.3, 4.3, 5.4, 6.4, 7.4, 8.7, 10.1, 11.6, 14.6)
+names(w) <- 3:14
+M <- 0.2
+B <- cohortBiomass(Ninit, w, M)
+barplot(B)
 
-N[1] <- Ninit
-for(i in 2:length(Z))
-{
-  N[i] <- N[i-1]*exp(-Z[i])
-}
+# For later consideration
+#Fsel <- c(0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1)
+#Ftime <- 0
 
-N
-foo <- function(N, w)
-{
-  c(N*w)
-}
+#Fcalc <- function(Fsel,Ftime){
+#  c(Fsel%o%Ftime )
+#}
 
-foo(N, w)
+#Fm <- Fcalc(Fsel, Ftime)
+#Fm
+
