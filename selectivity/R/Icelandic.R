161f91aa4a121bd9b5f63cff88df3820b9fe2b32
al-yanez <aleyanez.rubio@gmail.com>
2017-11-22 16:55:32 -0300

    editing some text

--- selectivity/R/Icelandic.R
+++ selectivity/R/Icelandic.R
@@ -4,16 +4,18 @@
 w <- read.csv('http://data.hafro.is/assmt/2017/cod/catch_weights.csv', check.names = F)
 head(w)
 
-#Average of wieght
+#Average of weight
 last <- w[which(w$Year>'2007'),]
 ave <- colMeans(last)
 w <- ave[-1]/1000
 
-
+#Call the function
 source('functions/cohortBiomass.R')
 
-cohortBiomass(136, w, 0.2 )
+#Using iinitial values
+#M = 0.2 and Ninit = 136
 
+cohortBiomass(136, w, 0.2 )
 B <- cohortBiomass(Ninit, w, M)
 barplot(B)
 
