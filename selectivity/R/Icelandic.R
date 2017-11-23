b2fab4742824ce5abc5007fabdb202cc6ee6f88a
Arni Magnusson <arnima@hafro.is>
2017-11-23 17:28:53 +0100

    Icelandic cohort biomass analysis starts at age N$"3", last ten cohorts

--- selectivity/R/Icelandic.R
+++ selectivity/R/Icelandic.R
@@ -1,23 +1,20 @@
-#Icelandic Cod
+## Icelandic Cod
 
 source("functions/cohortBiomass.R")
 
-#Read weight data and average the last ten years
-w <- read.csv('http://data.hafro.is/assmt/2017/cod/catch_weights.csv', check.names = F)
-head(w)
-w[w==0] <- NA
+## Read weight data and average the last ten years
+w <- read.csv("http://data.hafro.is/assmt/2017/cod/catch_weights.csv", check.names = FALSE)
 w <- tail(w, 10)
 w <- colMeans(w[-1]/1000)
 
 M <- 0.2
 
-N <- read.csv('http://data.hafro.is/assmt/2017/cod/nmat.csv', check.names = F)
-Ninit <- (tail(N[["1"]], 12))
+N <- read.csv("http://data.hafro.is/assmt/2017/cod/nmat.csv", check.names = FALSE)
+Ninit <- tail(N$"3", 10)
 Ninit <- mean(Ninit)
 
-
 B <- cohortBiomass(Ninit, w, M)
-pdf("Icelandic.pdf", 7, 6)
+pdf("icelandic.pdf", 7, 6)
 barplot(B, names=names(B), xlab="Age", ylab="Biomass (kt)",
         main="Biomass of average cohort, in the absence of fishing\n(Icelandic cod)")
 dev.off()
