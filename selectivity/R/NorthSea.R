6bfd405a7310cfe58aad2610096fadab28cbae8c
Arni Magnusson <arnima@hafro.is>
2017-11-26 23:22:39 +0100

    Catch and selectivity

--- selectivity/R/NorthSea.R
+++ selectivity/R/NorthSea.R
@@ -5,15 +5,12 @@ source("functions/read.html.R")
 
 url <- "https://stockassessment.org/datadisk/stockassessment/userdirs/user3/nscod_ass06_fc17/"
 
+## 1  Cohort biomass
+
 w <- read.ices(paste0(url, "data/sw.dat"))
 w[w==0] <- NA
 w <- tail(w[,1:10], 10)
 w <- colMeans(w)
-
-## cw <- read.ices(paste0(url, "data/cw.dat"))
-## cw[cw==0] <- NA
-## cw <- tail(cw[,1:10], 10)
-## cw <- colMeans(cw)
 
 M <- read.ices(paste0(url, "data/nm.dat"))
 M <- tail(M[,1:10],10)
@@ -24,7 +21,27 @@ N <- tail(N$"1", 10)
 N <- mean(N) / 1000
 
 B <- cohortBiomass(N, w, M)
-pdf("north-sea.pdf", 7, 6)
+pdf("north-sea-cohort.pdf", 7, 6)
 barplot(B, names=names(B), xlab="Age", ylab="Biomass (kt)",
         main="Biomass of average cohort, in the absence of fishing\n(North Sea cod)")
+dev.off()
+
+## 2  Catch and selectivity
+
+C <- read.ices(paste0(url, "data/cn.dat"))
+C <- tail(C[,1:10], 10)
+C <- colMeans(C) / 1000
+pdf("north-sea-catch.pdf", 7, 6)
+barplot(C, names=names(C), xlab="Age", ylab="Catch (millions)",
+        main="Average catch in numbers\n(North Sea)")
+dev.off()
+
+Fmort <- read.html(paste0(url, "res/xxx-00-00.00.00_tab3.html"))
+Fmort <- tail(Fmort[names(Fmort)!="Year"], 10)
+Fmort <- colMeans(Fmort)
+S <- Fmort / max(Fmort)
+pdf("north-sea-selectivity.pdf", 7, 6)
+plot(S, type="l", ylim=c(0,1.05), xlab="Age", ylab="Selectivity",
+     main="Average selectivity\n(North Sea)", xaxt="n", yaxs="i")
+axis(1, at=seq_along(S), labels=names(S))
 dev.off()
