767ec5842af10457b80bbd569790cfb72fca8f27
Colin Millar <colinpmillar@gmail.com>
2018-09-10 17:15:46 +0200

    Fit a range of SR models using msy package code

--- stock-recruitment/comparing_recruitment/plotting_ideas.R
+++ stock-recruitment/comparing_recruitment/plotting_ideas.R
@@ -6,6 +6,53 @@ library(dplyr)
 sr_data <-
   read.csv("https://data.marine.gov.scot/sites/default/files/Ova_to_ova_SR.csv") %>%
   filter(Site == "Girnock") %>%
-  rename(year = Year, ova = Ova.Stock, rec = Ova.Recruit) %>%
-  select(year, ova, rec)
+  rename(year = Year, ssb = Ova.Stock, rec = Ova.Recruit) %>%
+  select(year, ssb, rec) %>%
+  mutate(ssb = ssb * 10^-5, rec = rec * 10^-5) %>%
+  filter(complete.cases(.))
 
+plot(rec ~ ssb,
+     data = sr_data,
+     ylim = c(0, max(sr_data$ssb)),
+     xlim = c(0, max(sr_data$rec)),
+     las = 1, pch = 16, cex = 0.7)
+
+# fit a few models (externally)
+
+mods <- c("Ricker", "Bevholt", "smooth_hockey", "Segreg")
+
+pred_data <-
+  data.frame(
+    ssb = seq(.1, max(sr_data$ssb), length.out = 100)
+  )
+
+
+preds <-
+  sapply(mods,
+         function(mod) {
+    func <- get(mod, asNamespace("msy"))
+    # fit
+    opt <-
+      stats::nlminb(
+        msy:::initial(mod, sr_data),
+        function(param, ...) -1 * msy::llik(param, ...),
+        data = sr_data,
+        model = func,
+        logpar = TRUE)
+
+    # predict
+    par <- with(opt, list(a = exp(par[1]), b = exp(par[2]), cv = exp(par[3])))
+    exp( func(par, pred_data$ssb) )
+})
+
+# plot
+plot(rec ~ ssb,
+     data = sr_data,
+     ylim = c(0, max(sr_data$ssb)),
+     xlim = c(0, max(sr_data$rec)),
+     las = 1, pch = 16, cex = 0.7)
+
+for (i in 1:ncol(preds)) {
+  lines(preds[,i] ~ pred_data$ssb,
+        type = "l", col = "blue")
+}
