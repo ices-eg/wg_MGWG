bd2bdf81ccf53e7d8bb5eea028a93b68472444a6
Arni Magnusson <arnima@hafro.is>
2018-10-16 07:47:39 +0200

    Plus group flag

--- selectivity/R/iceland.R
+++ selectivity/R/iceland.R
@@ -5,18 +5,19 @@ source("functions/stdplot.R")
 path <- "../data/iceland"
 yrs <- 2008:2017
 ages <- as.character(3:14)
+plus <- FALSE
 
 ## 1  Cohort biomass
 
-N <- read("natage")
+N <- read("natage", path, plus)
 Ninit <- N$"3"[N$Year %in% yrs]
 Ninit <- mean(Ninit)
 
-M <- read("natmort")
+M <- read("natmort", path, plus)
 M <- M[M$Year %in% yrs,]
 M <- colMeans(M[ages])
 
-w <- read("wcatch")
+w <- read("wcatch", path, plus)
 w <- w[w$Year %in% yrs,]
 w <- colMeans(w[ages])
 
@@ -25,12 +26,12 @@ BPR <- cohortBiomass(exp(-0.4), M, w)  # 1 recruit at age 1 => 0.67 at age 3
 
 ## 2  Catch and selectivity
 
-C <- read("catage")
+C <- read("catage", path, plus)
 C <- C[C$Year %in% yrs,]
 C <- colMeans(C[ages])
 Cp <- C / sum(C)
 
-Fmort <- read("fatage")
+Fmort <- read("fatage", path, plus)
 Fmort <- Fmort[Fmort$Year %in% yrs,]
 Fmort <- colMeans(Fmort[ages])
 S <- Fmort / max(Fmort)
