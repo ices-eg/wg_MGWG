09d8269590fe8c9f0c43503bf68e9d720f18e475
Arni Magnusson <arnima@hafro.is>
2018-10-16 21:52:49 +0200

    Add catch and maturity

--- selectivity/R/all.R
+++ selectivity/R/all.R
@@ -26,22 +26,24 @@ col <- rich.colors(8)
 stocks <- c("Faroe Plateau", "Georges Bank", "Gulf of Maine", "Iceland",
             "NAFO 2J3KL", "NE Arctic", "North Sea", "W Baltic")
 
-pdf("all.pdf", 10, 10)
-layout(matrix(c(1,2,3,3), nrow=2, byrow=TRUE))
+pdf("all.pdf", 10, 20)
+layout(matrix(c(1,2,3,3,4,4,5,5), nrow=4, byrow=TRUE))
 
-plot(NA, xlim=c(1,14), ylim=c(0,16), yaxs="i",
+## Weight
+plot(NA, xlim=c(1,14), ylim=c(0,15), yaxs="i",
      xlab="Age", ylab="Weight (kg)", main="Average weight")
-stdline(faroe_plateau$w, lwd, col[1])
-stdline(georges_bank$w,  lwd, col[2])
-stdline(gulf_of_maine$w, lwd, col[3])
-stdline(iceland$w,       lwd, col[4])
-stdline(nafo_2j3kl$w,    lwd, col[5])
-stdline(ne_arctic$w,     lwd, col[6], to=14)
-stdline(north_sea$w,     lwd, col[7])
-stdline(w_baltic$w,      lwd, col[8])
+stdline(faroe_plateau$wcatch, lwd, col[1])
+stdline(georges_bank$wcatch,  lwd, col[2])
+stdline(gulf_of_maine$wcatch, lwd, col[3])
+stdline(iceland$wcatch,       lwd, col[4])
+stdline(nafo_2j3kl$wcatch,    lwd, col[5])
+stdline(ne_arctic$wcatch,     lwd, col[6], to=14)
+stdline(north_sea$wcatch,     lwd, col[7])
+stdline(w_baltic$wcatch,      lwd, col[8])
 legend("topleft", legend=stocks, bty="n", lty=1, lwd=lwd, col=col,
        inset=0.02, cex=0.8)
 
+## Biomass per recruit
 plot(NA, xlim=c(1,14), ylim=c(0,3.9), yaxs="i",
      xlab="Age", ylab="Biomass per recruit (kg)",
      main="Biomass per recruit, in the absence of fishing")
@@ -57,6 +59,7 @@ stdline(w_baltic$BPR,      lwd, col[8])
 legend("topright", legend=stocks, bty="n", lty=1, lwd=lwd, col=col,
        inset=0.02, cex=0.8)
 
+## Selectivity
 plot(NA, xlim=c(1,14), ylim=c(0,1.05), yaxs="i",
      xlab="Age", ylab="Selectivity", main="Average selectivity")
 stdline(faroe_plateau$S, lwd, col[1])
@@ -68,6 +71,36 @@ stdline(ne_arctic$S,     lwd, col[6], to=14)
 stdline(north_sea$S,     lwd, col[7])
 stdline(w_baltic$S,      lwd, col[8])
 legend("topright", legend=stocks, bty="n", lty=1, lwd=lwd, col=col,
+       inset=0.02, cex=0.8)
+box()
+
+## Catch
+plot(NA, xlim=c(1,14), ylim=c(0,0.4), yaxs="i",
+     xlab="Age", ylab="Proportion of catch", main="Catch composition")
+stdline(faroe_plateau$Cp, lwd, col[1])
+stdline(georges_bank$Cp,  lwd, col[2])
+stdline(gulf_of_maine$Cp, lwd, col[3])
+stdline(iceland$Cp,       lwd, col[4])
+stdline(nafo_2j3kl$Cp,    lwd, col[5])
+stdline(ne_arctic$Cp,     lwd, col[6], to=14)
+stdline(north_sea$Cp,     lwd, col[7], to=8)
+stdline(w_baltic$Cp,      lwd, col[8])
+legend("topright", legend=stocks, bty="n", lty=1, lwd=lwd, col=col,
+       inset=0.02, cex=0.8)
+box()
+
+## Maturity
+plot(NA, xlim=c(1,14), ylim=c(0,1.05), yaxs="i",
+     xlab="Age", ylab="Proportion mature", main="Maturity")
+stdline(faroe_plateau$mat, lwd, col[1])
+stdline(georges_bank$mat,  lwd, col[2])
+stdline(gulf_of_maine$mat, lwd, col[3])
+stdline(iceland$mat,       lwd, col[4])
+stdline(nafo_2j3kl$mat,    lwd, col[5])
+stdline(ne_arctic$mat,     lwd, col[6], from=4, to=14)
+stdline(north_sea$mat,     lwd, col[7])
+stdline(w_baltic$mat,      lwd, col[8])
+legend("bottomright", legend=stocks, bty="n", lty=1, lwd=lwd, col=col,
        inset=0.02, cex=0.8)
 box()
 
