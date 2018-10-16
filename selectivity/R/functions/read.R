c458485048bacb1ee3fd6f0b0ecf4012a347f548
Arni Magnusson <arnima@hafro.is>
2018-10-16 13:01:58 +0200

    read(name, path, plus) to read data

--- selectivity/R/functions/read.R
+++ selectivity/R/functions/read.R
@@ -1,7 +1,15 @@
-read <- function(basename, plus=FALSE)
+read <- function(name, path=".", plus=FALSE)
 {
-  tab <- read.csv(paste0(file.path(path,basename),".csv"), check.names=FALSE)
+  tab <- read.csv(paste0(file.path(path,name),".csv"), check.names=FALSE)
   if(plus)
     names(tab) <- sub("\\+$", "", names(tab))
   tab
 }
+
+## Example
+## path <- "../data/iceland"
+## plus <- FALSE
+## read("catage", path, plus)
+##
+## The 'plus' argument indicates whether the last column is a plus group that
+## will be treated as a normal age class.
