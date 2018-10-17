bfdfc9f5a0e1577e37465704b8430fc32d145b1c
Arni Magnusson <arnima@hafro.is>
2018-10-17 17:26:25 +0200

    Profile Fmax and F0.1, Y and SSB

--- selectivity/R/profile.R
+++ selectivity/R/profile.R
@@ -1,17 +1,77 @@
-# Effects of alternative selectivity patterns
+## Effects of alternative selectivity patterns
 
 source("functions/applyFmax.R")
+source("functions/applyF0.1.R")
+source("functions/profSlide.R")
+source("functions/slide.R")
 
 ## 1  Import
 
 setwd("stocks")
+source("faroe_plateau.R")
+source("georges_bank.R")
+source("gulf_of_maine.R")
 source("iceland.R")
+source("nafo_2j3kl.R")
+source("ne_arctic.R")
+source("north_sea.R")
+source("w_baltic.R")
 setwd("..")
 
-## Current
-applyFmax(Ninit, M, S, mat, wcatch)
+################################################################################
 
-## Shifted
-stdplot(S, "Shifting the selectivity", "Selectivity", type="o")
-grid()
-lines(seq(3.5,14.5), S, lty=3)
+profPlot <- function(stock, shift, strategy=applyFmax, focus="Y",
+                     xlim=range(shift), ylim=NULL)
+{
+  main <- deparse(substitute(stock))
+  ylab <- switch(focus, Y="Potential yield (kt)", SSB="SSB (kt)")
+  stock$profile <- profSlide(stock, shift, strategy, focus)
+  plot(stock$profile, type="l", xlim=xlim, ylim=ylim, yaxs="i",
+       xlab="Age shift (yrs)", ylab=ylab, main=main)
+  points(stock$profile[stock$profile$shift==0,])
+}
+
+shift <- list(fp=-1:8, gb=-1:8, gm=-1:7, ic=-1:9,
+              n2=-1:9, ne=-1:9, ns=-1:9, wb=-1:6)
+
+par(mfrow=c(3,3))
+profPlot(faroe_plateau, shift$fp, applyFmax, "Y", c(-1,9), c(0,14))
+profPlot(georges_bank,  shift$gb, applyFmax, "Y", c(-1,9), c(0,10))
+profPlot(gulf_of_maine, shift$gm, applyFmax, "Y", c(-1,9), c(0,4))
+profPlot(iceland,       shift$ic, applyFmax, "Y", c(-1,9), c(0,350))
+profPlot(nafo_2j3kl,    shift$n2, applyFmax, "Y", c(-1,9), c(0,100))
+profPlot(ne_arctic,     shift$ne, applyFmax, "Y", c(-1,9), c(0,900))
+profPlot(north_sea,     shift$ns, applyFmax, "Y", c(-1,9), c(0,100))
+profPlot(w_baltic,      shift$wb, applyFmax, "Y", c(-1,9), c(0,35))
+
+par(mfrow=c(3,3))
+profPlot(faroe_plateau, shift$fp, applyFmax, "SSB", c(-1,9), c(0,100))
+profPlot(georges_bank,  shift$gb, applyFmax, "SSB", c(-1,9), c(0,70))
+profPlot(gulf_of_maine, shift$gm, applyFmax, "SSB", c(-1,9), c(0,20))
+profPlot(iceland,       shift$ic, applyFmax, "SSB", c(-1,9), c(0,2500))
+profPlot(nafo_2j3kl,    shift$n2, applyFmax, "SSB", c(-1,9), c(0,500))
+profPlot(ne_arctic,     shift$ne, applyFmax, "SSB", c(-1,9), c(0,6000))
+profPlot(north_sea,     shift$ns, applyFmax, "SSB", c(-1,9), c(0,700))
+profPlot(w_baltic,      shift$wb, applyFmax, "SSB", c(-1,9), c(0,160))
+
+################################################################################
+
+par(mfrow=c(3,3))
+profPlot(faroe_plateau, shift$fp, applyF0.1, "Y", c(-1,9), c(0,14))
+profPlot(georges_bank,  shift$gb, applyF0.1, "Y", c(-1,9), c(0,10))
+profPlot(gulf_of_maine, shift$gm, applyF0.1, "Y", c(-1,9), c(0,4))
+profPlot(iceland,       shift$ic, applyF0.1, "Y", c(-1,9), c(0,350))
+profPlot(nafo_2j3kl,    shift$n2, applyF0.1, "Y", c(-1,9), c(0,100))
+profPlot(ne_arctic,     shift$ne, applyF0.1, "Y", c(-1,9), c(0,900))
+profPlot(north_sea,     shift$ns, applyF0.1, "Y", c(-1,9), c(0,100))
+profPlot(w_baltic,      shift$wb, applyF0.1, "Y", c(-1,9), c(0,35))
+
+par(mfrow=c(3,3))
+profPlot(faroe_plateau, shift$fp, applyF0.1, "SSB", c(-1,9), c(0,100))
+profPlot(georges_bank,  shift$gb, applyF0.1, "SSB", c(-1,9), c(0,70))
+profPlot(gulf_of_maine, shift$gm, applyF0.1, "SSB", c(-1,9), c(0,20))
+profPlot(iceland,       shift$ic, applyF0.1, "SSB", c(-1,9), c(0,2500))
+profPlot(nafo_2j3kl,    shift$n2, applyF0.1, "SSB", c(-1,9), c(0,500))
+profPlot(ne_arctic,     shift$ne, applyF0.1, "SSB", c(-1,9), c(0,6000))
+profPlot(north_sea,     shift$ns, applyF0.1, "SSB", c(-1,9), c(0,700))
+profPlot(w_baltic,      shift$wb, applyF0.1, "SSB", c(-1,9), c(0,160))
