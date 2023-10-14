c452c142d9f9ad70682e5d8ec42d29b34cecf7f8
Arni Magnusson <thisisarni@gmail.com>
2023-10-15 03:14:51 +1100

    Calculate summary table

--- selectivity/R/chapter/model.R
+++ selectivity/R/chapter/model.R
@@ -1,9 +1,30 @@
 ## Run analysis, write model results
 
-## Before:
+## Before: stocks.RData (data)
 ## After:
 
 library(TAF)
+source("boot/software/A50.R")
+source("boot/software/Abar.R")
+source("utilities.R")
 
 mkdir("model")
 
+load("data/stocks.RData")
+tonnes <- read.taf("data/tonnes.csv")
+
+## 1  Prepare table
+summary <- data.frame(id=names(tonnes)[-1])
+summary$Stock <- c("Eastern Baltic", "Faroe Plateau", "Georges Bank",
+                   "Greenland inshore", "Gulf of Maine", "Iceland", "Irish Sea",
+                   "Kattegat", "NAFO 2J3KL", "NAFO 3M", "NAFO 3NO", "NAFO 3Ps",
+                   "Northeast Arctic", "North Sea", "Norway coastal",
+                   "Southern Celtic", "Western Baltic")
+summary$Years <- sapply(summary$id, years)
+summary$Catch <- sapply(summary$id, catch)
+summary$AbarCatch <- sapply(summary$id, abar_catch)
+summary$A50mat <- sapply(summary$id, a50mat)
+summary$W5 <- sapply(summary$id, w5)
+
+## 2  Write table
+write.taf(summary, dir="model")
