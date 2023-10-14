3692bac86d8073e21bab4b09cfbd514c060f968c
Arni Magnusson <thisisarni@gmail.com>
2023-10-15 03:10:56 +1100

    Add tonnes.csv

--- selectivity/R/chapter/data.R
+++ selectivity/R/chapter/data.R
@@ -1,10 +1,11 @@
 ## Preprocess data, write TAF data tables
 
-## Before: stocks.RData (boot/data/stocks)
-## After:  stocks.RData (data)
+## Before: tonnes.csv (boot/data), stocks.RData (boot/data/stocks)
+## After:  stocks.RData, tonnes.csv (data)
 
 library(TAF)
 
 mkdir("data")
 
 cp("boot/data/stocks/stocks.RData", "data")
+cp("boot/data/tonnes.csv", "data")
