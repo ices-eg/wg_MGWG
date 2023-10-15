f8c2f527277e5f55727bc643710ba6fa7558409b
Arni Magnusson <thisisarni@gmail.com>
2023-10-16 02:51:59 +1100

    Plot Abar vs A50mat

--- selectivity/R/chapter/report.R
+++ selectivity/R/chapter/report.R
@@ -1,9 +1,28 @@
 ## Prepare plots and tables for report
 
-## Before:
-## After:
+## Before: summary.csv (output)
+## After:  abar_vs_a50mat.pdf (report)
 
 library(TAF)
+library(arni)  # eps, eps2pdf
 
 mkdir("report")
 
+# 1  Read summary table
+summary <- read.taf("output/summary.csv")
+
+# 2  Prepare plot labels
+summary$Label <- c("E Baltic", "Faroe", "Georges", "Greenland", "Maine",
+                   "Iceland", "Irish", "Kattegat", "2J3KL", "3M", "3NO", "3Ps",
+                   "NE Arctic", "North Sea", "Norway", "Celtic", "W Baltic")
+
+# 3  Plot Abar~A50mat
+filename <- "report/abar_vs_a50mat.eps"
+eps(filename, width=6, height=6)
+plot(NA, xlim=c(0,8), ylim=c(0,8), xlab="Age at 50% maturity",
+     ylab="Average age in catches")
+abline(a=0, b=1, lty=3, lwd=2.5, col="gray")
+text(AbarCatch~A50mat, data=summary, labels=Label, cex=0.8)
+dev.off()
+eps2pdf(filename)
+file.remove(filename)
