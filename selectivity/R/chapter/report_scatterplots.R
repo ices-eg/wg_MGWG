a9ada2f63a88d5a31f25b38a09f6e40065fa0ce8
Arni Magnusson <thisisarni@gmail.com>
2023-10-16 12:54:59 +1100

    Plot W5 vs. A50mat

--- selectivity/R/chapter/report_scatterplots.R
+++ selectivity/R/chapter/report_scatterplots.R
@@ -1,7 +1,7 @@
 ## Prepare scatterplots
 
 ## Before: summary.csv (output)
-## After:  abar_vs_a50mat.pdf (report)
+## After:  abar_vs_a50mat.pdf, w5_vs_a50mat.pdf (report)
 
 library(TAF)
 
@@ -22,4 +22,13 @@ plot(NA, xlim=c(0,8), ylim=c(0,8), xlab="Age at 50% maturity",
      ylab="Average age in catches")
 abline(a=0, b=1, lty=3, lwd=2.5, col="gray")
 text(AbarCatch~A50mat, data=summary, labels=Label, cex=0.8)
+dev.off()
+
+# 4  Plot W5~A50mat
+pdf("report/w5_vs_a50mat.pdf", width=6, height=6)
+par(plt=c(0.15, 0.97, 0.15, 0.97))
+plot(NA, xlim=c(0,8), ylim=c(0,8), xlab="Age at 50% maturity",
+     ylab="Average weight at age 5 (kg)")
+abline(a=0, b=1, lty=3, lwd=2.5, col="gray")
+text(W5~A50mat, data=summary, labels=Label, cex=0.8)
 dev.off()
