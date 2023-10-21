ee2888e16bd304a797d334eb4f68541f06f369c6
Arni Magnusson <thisisarni@gmail.com>
2023-10-21 18:19:40 +1100

    Produce stocks.rds

--- selectivity/R/chapter/boot/stocks.R
+++ selectivity/R/chapter/boot/stocks.R
@@ -1,6 +1,7 @@
 ## Import stocks
 
-## owd <- setwd("../../stocks")
+# Read stocks
+# owd <- setwd("../../stocks")
 owd <- setwd("../../../../stocks")
 source("e_baltic.R")
 source("faroe_plateau.R")
@@ -20,8 +21,27 @@ source("norway.R")
 source("s_celtic.R")
 source("w_baltic.R")
 setwd(owd)
-
 rm(ages, B, BPR, C, cohortBiomass, Cp, dims, Fmort, M, mat, minage, N, Ninit,
    owd, path, plus, read, S, stdplot, wcatch, wstock, yrs)
 
-save(list=ls(), file="stocks.RData")
+# Construct list
+stocks <- list(e_baltic=e_baltic,
+               e_baltic=e_baltic,
+               faroe_plateau=faroe_plateau,
+               georges_bank=georges_bank,
+               greenland=greenland,
+               gulf_of_maine=gulf_of_maine,
+               iceland=iceland,
+               irish_sea=irish_sea,
+               kattegat=kattegat,
+               nafo_2j3kl=nafo_2j3kl,
+               nafo_3m=nafo_3m,
+               nafo_3no=nafo_3no,
+               nafo_3ps=nafo_3ps,
+               ne_arctic=ne_arctic,
+               north_sea=north_sea,
+               norway=norway,
+               s_celtic=s_celtic,
+               w_baltic=w_baltic)
+
+saveRDS(stocks, file="stocks.rds")
--- selectivity/R/chapter/data.R
+++ selectivity/R/chapter/data.R
@@ -1,11 +1,12 @@
 ## Preprocess data, write TAF data tables
 
-## Before: tonnes.csv (boot/data), stocks.RData (boot/data/stocks)
+## Before: tonnes.csv (boot/data), stocks.rds (boot/data/stocks)
 ## After:  stocks.RData, tonnes.csv (data)
 
 library(TAF)
 
 mkdir("data")
 
-cp("boot/data/stocks/stocks.RData", "data")
+# Copy from boot to data
+cp("boot/data/stocks/stocks.rds", "data")
 cp("boot/data/tonnes.csv", "data")
