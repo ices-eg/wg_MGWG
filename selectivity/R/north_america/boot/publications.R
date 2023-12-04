e63b93346b558ac1024c36fb46e16c02b3c1af9a
Arni Magnusson <thisisarni@gmail.com>
2023-12-04 18:04:51 +1300

    TAF workflow to download publications

--- selectivity/R/north_america/boot/publications.R
+++ selectivity/R/north_america/boot/publications.R
@@ -1,40 +1,30 @@
 library(TAF)
 
-message("Downloading advice sheets:")
+message("Downloading publications:")
 
-message("2020 Norway")
-download("https://ices-library.figshare.com/ndownloader/files/33415943",
-         destfile="advice_norway.pdf")
+# NAFO
+message("NAFO 2022 Northern")
+download("https://waves-vagues.dfo-mpo.gc.ca/library-bibliotheque/41078457.pdf",
+         destfile="nafo_northern.pdf")
+message("NAFO 2023 Flemish")
+download("https://www.nafo.int/Portals/0/PDFs/Advice/2023/cod3m.pdf",
+         destfile="nafo_flemish.pdf")
+message("NAFO 2021 Grand")
+download("https://www.nafo.int/Portals/0/PDFs/Advice/2021/cod3no.pdf",
+         destfile="nafo_grand.pdf")
+message("NAFO 2022 Pierre")
+download("https://waves-vagues.dfo-mpo.gc.ca/library-bibliotheque/41063715.pdf",
+         destfile="nafo_pierre.pdf")
 
-message("2021 NE Arctic")
-download("https://ices-library.figshare.com/ndownloader/files/33417911",
-         destfile="advice_ne_arctic.pdf")
+# NOAA
+smart <- "https://apps-st.fisheries.noaa.gov/sis/docServlet?fileAction=download"
+message("NOAA 2021 Georges")
+download(paste0(smart, "&fileId=7470"), destfile="noaa_georges.pdf")
+message("NOAA 2021 Maine")
+download(paste0(smart, "&fileId=7550"), destfile="noaa_maine.pdf")
 
-message("2022 Faroe")
-download("https://ices-library.figshare.com/ndownloader/files/38370326",
-         destfile="advice_faroe.pdf")
-message("2022 Greenland")
-download("https://ices-library.figshare.com/ndownloader/files/35905118",
-         destfile="advice_greenland.pdf")
-message("2022 North Sea")
-download("https://ices-library.figshare.com/ndownloader/files/38140326",
-         destfile="advice_north_sea.pdf")
-message("2022 W Baltic")
-download("https://ices-library.figshare.com/ndownloader/files/35457407",
-         destfile="advice_w_baltic.pdf")
+# Map
+message("Map")
+download(file.path("https://www.nafo.int/Portals/0/PDFs/GeneralInfo",
+                   "NAFO%20map-poster-8.5x11.pdf"), destfile="map.pdf")
 
-message("2023 E Baltic")
-download("https://ices-library.figshare.com/ndownloader/files/41007086",
-         destfile="advice_e_baltic.pdf")
-message("2023 Iceland")
-download("https://ices-library.figshare.com/ndownloader/files/41156720",
-         destfile="advice_iceland.pdf")
-message("2023 Irish")
-download("https://ices-library.figshare.com/ndownloader/files/43001974",
-         destfile="advice_irish.pdf")
-message("2023 Kattegat")
-download("https://ices-library.figshare.com/ndownloader/files/41385474",
-         destfile="advice_kattegat.pdf")
-message("2023 S Celtic")
-download("https://ices-library.figshare.com/ndownloader/files/41412585",
-         destfile="advice_s_celtic.pdf")
