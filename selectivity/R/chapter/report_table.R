00e99ccfc60fac4124f4243629d0a43bdc1ef229
Arni Magnusson <thisisarni@gmail.com>
2023-12-05 05:15:03 +1100

    Summary table comparing the stocks

--- selectivity/R/chapter/report_table.R
+++ selectivity/R/chapter/report_table.R
@@ -12,7 +12,29 @@ summary <- read.taf("output/summary.csv")
 summary$id <- summary$Label <- NULL
 summary$Catch <- round(summary$Catch, -2)
 summary <- rnd(summary, 4:6, 1)
+names(summary)[names(summary) == "Years"] <- "Decade"
 names(summary)[names(summary) == "AbarCatch"] <- "Abar"
+
+# Sort rows
+z <- c("Greenland inshore",
+       "Northeast Arctic",
+       "Iceland",
+       "Faroe Plateau",
+       "Norway coastal",
+       "North Sea",
+       "Kattegat",
+       "Western Baltic",
+       "Eastern Baltic",
+       "Irish Sea",
+       "Southern Celtic",
+       "Northern",
+       "Flemish Cap",
+       "Grand Bank",
+       "St Pierre",
+       "Gulf of Maine",
+       "Georges Bank")
+rownames(summary) <- summary$Stock
+summary <- summary[z,]
 
 # Write report table
 write.taf(summary, dir="report")
