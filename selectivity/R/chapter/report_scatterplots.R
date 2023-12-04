e64924fe9b688a04f1aec98d2617993e1f33b8d7
Arni Magnusson <thisisarni@gmail.com>
2023-12-05 02:19:14 +1100

    Nudge labels so they don't overlap

--- selectivity/R/chapter/report_scatterplots.R
+++ selectivity/R/chapter/report_scatterplots.R
@@ -10,19 +10,73 @@ mkdir("report")
 # Read summary table
 summary <- read.taf("output/summary.csv")
 
+# Nudge labels so they don't overlap
+p1 <- summary[c("A50mat", "AbarCatch")]  # plot 1
+p2 <- summary[c("A50mat", "W5")]         # plot 2
+names(p1) <- names(p2) <- c("x", "y")
+rownames(p1) <- rownames(p2) <- summary$Label
+
+nudge <- function(x, n, dx=0, dy=0.25)
+{
+  x[n,] <- x[n,] + c(dx, dy)
+  x
+}
+p1 <- nudge(p1, "Celtic", -0.35, 0.2)
+p1 <- nudge(p1, "E Baltic", -0.25, -0.25)
+p1 <- nudge(p1, "Faroe", 0.1, -0.25)
+p1 <- nudge(p1, "Flemish", -0.2)
+p1 <- nudge(p1, "Georges", -0.5, 0.2)
+p1 <- nudge(p1, "Grand")
+p1 <- nudge(p1, "Greenland", -0.1)
+p1 <- nudge(p1, "Iceland")
+p1 <- nudge(p1, "Irish", -0.35, 0)
+p1 <- nudge(p1, "Kattegat", 0, -0.25)
+p1 <- nudge(p1, "Maine", 0.2)
+p1 <- nudge(p1, "NE Arctic")
+p1 <- nudge(p1, "North Sea", 0.5, -0.25)
+p1 <- nudge(p1, "Northern", 0, -0.25)
+p1 <- nudge(p1, "Norway")
+p1 <- nudge(p1, "Pierre")
+p1 <- nudge(p1, "W Baltic")
+
+nudge <- function(x, n, dx=0, dy=0.28)
+{
+  x[n,] <- x[n,] + c(dx, dy)
+  x
+}
+p2 <- nudge(p2, "Celtic")
+p2 <- nudge(p2, "E Baltic")
+p2 <- nudge(p2, "Faroe", 0.43, 0)
+p2 <- nudge(p2, "Flemish")
+p2 <- nudge(p2, "Georges", -0.6, 0)
+p2 <- nudge(p2, "Grand", 0.45, 0)
+p2 <- nudge(p2, "Greenland", -0.68, 0)
+p2 <- nudge(p2, "Iceland")
+p2 <- nudge(p2, "Irish")
+p2 <- nudge(p2, "Kattegat")
+p2 <- nudge(p2, "Maine", 0.1, -0.28)
+p2 <- nudge(p2, "NE Arctic")
+p2 <- nudge(p2, "North Sea")
+p2 <- nudge(p2, "Northern", -0.5, -0.2)
+p2 <- nudge(p2, "Norway")
+p2 <- nudge(p2, "Pierre")
+p2 <- nudge(p2, "W Baltic", -0.1, -0.28)
+
 # Plot Abar~A50mat
 pdf("report/abar_vs_a50mat.pdf", width=6, height=6)
 par(plt=c(0.15, 0.97, 0.15, 0.97))
 plot(NA, xlim=c(0,8), ylim=c(0,8), xlab="Age at 50% maturity",
      ylab="Average age in catches")
 abline(a=0, b=1, lty=3, lwd=2.5, col="gray")
-text(AbarCatch~A50mat, data=summary, labels=Label, cex=0.8)
+points(AbarCatch~A50mat, data=summary, pch=16)
+text(y~x, data=p1, labels=rownames(p1), cex=0.8)
 dev.off()
 
 # Plot W5~A50mat
 pdf("report/w5_vs_a50mat.pdf", width=6, height=6)
 par(plt=c(0.15, 0.97, 0.15, 0.97))
-plot(NA, xlim=c(0,8), ylim=c(0,8), xlab="Age at 50% maturity",
+plot(NA, xlim=c(0,8), ylim=c(0,10), xlab="Age at 50% maturity",
      ylab="Average weight at age 5 (kg)")
-text(W5~A50mat, data=summary, labels=Label, cex=0.8)
+points(W5~A50mat, data=summary, pch=16)
+text(y~x, data=p2, labels=rownames(p2), cex=0.8)
 dev.off()
