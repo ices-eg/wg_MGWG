a1afe66ac684186067b6ea5636d7e92b51857405
Arni Magnusson <thisisarni@gmail.com>
2023-12-10 03:39:45 +1100

    Remove Kattegat and St Pierre from scatterplot

--- selectivity/R/chapter/report_scatterplots.R
+++ selectivity/R/chapter/report_scatterplots.R
@@ -10,6 +10,11 @@ mkdir("report")
 # Read summary table
 summary <- read.taf("output/summary.csv")
 
+# Remove Kattegat and St Pierre
+summary <- summary[summary$id != "kattegat",]
+summary <- summary[summary$id != "nafo_3ps",]
+rownames(summary) <- NULL
+
 # Nudge labels so they don't overlap
 p1 <- summary[c("A50mat", "AbarCatch")]  # plot 1
 p2 <- summary[c("A50mat", "W5")]         # plot 2
@@ -29,13 +34,11 @@ p1 <- nudge(p1, "Grand")
 p1 <- nudge(p1, "Greenland", -0.1)
 p1 <- nudge(p1, "Iceland")
 p1 <- nudge(p1, "Irish", -0.35, 0)
-p1 <- nudge(p1, "Kattegat", 0, -0.25)
 p1 <- nudge(p1, "Maine", 0.15)
 p1 <- nudge(p1, "NE Arctic")
 p1 <- nudge(p1, "North Sea", 0.5, -0.25)
-p1 <- nudge(p1, "Northern", 0, -0.25)
+p1 <- nudge(p1, "Northern")
 p1 <- nudge(p1, "Norway")
-p1 <- nudge(p1, "Pierre")
 p1 <- nudge(p1, "S Celtic", -0.35, 0.2)
 p1 <- nudge(p1, "W Baltic")
 
@@ -48,17 +51,15 @@ p2 <- nudge(p2, "E Baltic")
 p2 <- nudge(p2, "Faroe", 0.43, 0)
 p2 <- nudge(p2, "Flemish")
 p2 <- nudge(p2, "Georges", -0.6, 0)
-p2 <- nudge(p2, "Grand", 0.45, 0)
+p2 <- nudge(p2, "Grand", 0.1, -0.28)
 p2 <- nudge(p2, "Greenland", -0.68, 0)
 p2 <- nudge(p2, "Iceland")
 p2 <- nudge(p2, "Irish")
-p2 <- nudge(p2, "Kattegat")
 p2 <- nudge(p2, "Maine", 0.07, -0.28)
 p2 <- nudge(p2, "NE Arctic")
 p2 <- nudge(p2, "North Sea")
-p2 <- nudge(p2, "Northern", -0.5, -0.2)
+p2 <- nudge(p2, "Northern", -0.1)
 p2 <- nudge(p2, "Norway")
-p2 <- nudge(p2, "Pierre")
 p2 <- nudge(p2, "S Celtic")
 p2 <- nudge(p2, "W Baltic", -0.1, -0.28)
 
